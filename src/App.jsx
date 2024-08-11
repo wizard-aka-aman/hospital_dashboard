5847dc7b35237c8730e6c257cffbcfafcac5ae34
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-08-11 12:58:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/admin/me",
+          "https://hospital-backend-trs5.onrender.com/api/v1/user/admin/me",
           {
             withCredentials: true,
           }
