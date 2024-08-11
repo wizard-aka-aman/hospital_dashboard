e1269bf8cfc8bf45a93381712740764fbee4c633
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-08-11 13:01:03 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
     e.preventDefault();
     try {
       await axios.post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://hospital-backend-trs5.onrender.com/api/v1/user/login",
           { email, password, confirmPassword, role: "Admin" },
           {
             withCredentials: true,
