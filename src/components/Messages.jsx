fdec1c81437d663d241abb4adc41e6a205a40c1f
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-08-11 13:01:23 +0530

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -12,7 +12,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/message/getall",
+          "https://hospital-backend-trs5.onrender.com/api/v1/message/getall",
           { withCredentials: true }
         );
         setMessages(data.messages);
